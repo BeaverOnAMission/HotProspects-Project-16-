77a217f31d022c9dc30450e4984d8efe83a3529f
BeaverOnAMission <babenko.bohdan01@gmail.com>
2024-03-02 22:15:55 +0200

    initial

---
+++ HotProspects(Project 16)/NotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationView.swift
+//  HotProspects(Project 16)
+//
+//  Created by mac on 02.03.2024.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationView()
+}
